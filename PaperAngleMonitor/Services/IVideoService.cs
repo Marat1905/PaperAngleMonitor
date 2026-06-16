22f9b7ae9f9eff9ae6899237a0eef9da8f59bfd9
Marat <milya010790@mail.ru>
2026-06-16 09:34:46 +0500

    Подправили

--- PaperAngleMonitor/Services/IVideoService.cs
+++ PaperAngleMonitor/Services/IVideoService.cs
@@ -1,4 +1,5 @@
 ﻿using OpenCvSharp;
+using PaperAngleMonitor.Models;
 
 namespace PaperAngleMonitor.Services
 {
@@ -9,10 +10,13 @@ namespace PaperAngleMonitor.Services
         double CurrentFps { get; }
 
         Task<bool> ConnectAsync();
-
         Task DisconnectAsync();
         Task StartCaptureAsync();
         Task StopCaptureAsync();
 
+        void ApplySettings(BaslerSettings settings);
+        (double Min, double Max) GetExposureRange();
+        (double Min, double Max) GetGainRange();
+        (double Min, double Max) GetFrameRateRange();
     }
 }
